53fcadf7c7f3a5d2a19a6b34fcff686a64658d70
Guy Martin <gmsoft@tuxicoman.be>
2024-08-11 23:02:00 +0200

    Add getop support.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,51 @@
-
+extern crate getopts;
+use getopts::Options;
 use pcap::{Capture,Linktype};
+use std::env;
 
 pub mod proto;
 
 use crate::proto::ProtoParser;
 use crate::proto::ProtoStackEntry;
 
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
 
 fn main() {
 
 
-    let mut cap = Capture::from_file("tftp.cap").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optopt("r", "read", "input PCAP file", "NAME");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]){
+        Ok(m) => { m }
+        Err(f) => { panic!("{}", f.to_string()) }
+    };
+
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+
+    let filename = match matches.opt_str("r") {
+        None => { panic!("No filename provided") }
+        Some(f) => { f }
+    };
+
+    let mut cap = Capture::from_file(filename).unwrap();
 
 
     let datalink = cap.get_datalink();
     println!("Capture datalink : {:?}", datalink);
 
     while let Ok(packet) = cap.next_packet() {
-
 
         process_packet(packet.data, datalink);
     }
